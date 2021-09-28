cd265378c2ac405ebdd89929868ea472a9cc601f
celine-ece <celine.mathon@hotmail.fr>
2021-09-28 12:13:14 +0200

    jusqu'a 3

--- js/script.js
+++ js/script.js
@@ -29,3 +29,35 @@ function start() {
       console.error(error);
     });
 }
+
+// fonction recupération ville
+function GetCity(){
+ var city= document.getElementById('city-input').value;
+ alert(city);
+ 
+ const apiWeathern = new API_WEATHER(city)
+apiWeathern.fetchTodayForecast()
+.then(function(response) {
+  // Récupère la donnée d'une API
+  const data = response.data;
+
+  // On récupère l'information principal
+  const main = data.weather[0].main;
+  const description = data.weather[0].description;
+  const temp = data.main.temp;
+  const icon = apiWeathern.getHTMLElementFromIcon(data.weather[0].icon);
+  console.log(main);
+  console.log(description);
+  // Modifier le DOM
+  document.getElementById('today-forecast-main').innerHTML = main;
+  document.getElementById('today-forecast-more-info').innerHTML = description;
+  document.getElementById('icon-weather-container').innerHTML = icon;
+  document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
+  
+})
+.catch(function(error) {
+  // Affiche une erreur
+  console.error(error);
+});
+ 
+}
